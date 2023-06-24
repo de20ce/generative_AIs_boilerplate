0dbac64882d13bf734e9acbd9d88fd1df6a56317
Vivien Ogoun <vivienogoun1@gmail.com>
2023-06-24 20:31:34 +0100

    Create chatbot page and add dummy chatbot action management

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+export default function App() {
+  const [id, setId] = useState(1)
+  const [messageList, setMessageList] = useState([{ id: 0, message: 'Salut!', fromUser: false }])
+  const [userMessage, setUserMessage] = useState('')
 
-export default App;
+  function handleClick() {
+    const newUserMessage = { id: id, message: userMessage, fromUser: true}
+    const newBotMessage = { id: `${id+1}`, message: 'Je suis bête', fromUser: false}
+    setMessageList([...messageList, newUserMessage, newBotMessage])
+    setUserMessage('')
+    setId(id + 2)
+  }
+
+  return <div className='app'>
+    <div className='container'>
+      {
+        messageList.map(({id, message, fromUser}) => <Message key={id} message={message} fromUser={fromUser}/>)
+      }
+    </div>
+    <textarea value={userMessage} onChange={(e) => setUserMessage(e.target.value)} rows={3} placeholder='Envoyer un message' autoFocus/>
+    <button onClick={handleClick} disabled={userMessage === ''}>Envoyer</button>
+  </div>
+};
+
+function Message({ message, fromUser }) {
+  return <div className={fromUser ? 'message-from-user' : 'message-from-bot'}>
+    <p>{message}</p>
+  </div>
+}
