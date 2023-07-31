59f8e273c36a72d9673646c1b60762ab4f28cd08
Adonislab <nobimetanguy19@gmail.com>
2023-07-31 11:23:05 +0100

    I make text to text application version 1

--- core/text2text/consumers.py
+++ core/text2text/consumers.py
@@ -1,37 +1,39 @@
-
-import json
-
 from asgiref.sync import async_to_sync
+from celery import shared_task
 from channels.generic.websocket import WebsocketConsumer
+from transformers import pipeline
+import json
+import logging
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
+# Load the template and tokenizer
+generator = pipeline("text-generation", model='distilgpt2')
+
+from .tasks import get_response
 class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
+        logging.info(f'Received text: {text_data}')
 
-        # The consumer ChatConsumer is synchronous while the channel layer
-        # methods are asynchronous. Therefore wrap the methods in async-to-sync
+        text_data_json = json.loads(text_data)
+        logging.info(f'just before sending to engine: {text_data_json}')
+
+        # Client input
+        user_input = text_data_json.get('text', '')
+
+        if user_input:
+            # Call the task synchronously
+            response_data = get_response(user_input)
+        else:
+            response_data = "Please, you should enter a text"
+
         async_to_sync(self.channel_layer.send)(
             self.channel_name,
             {
                 "type": "chat_message",
-                "text": {"msg": text_data_json["text"], "source": "user"},
-            },
+                "text": [{"msg": response_data, "source": "bot"}],
+            }
         )
 
-        # We will later replace this call with a celery task that will
-        # use a Python library called ChatterBot to generate an automated
-        # response to a user's input.
-        async_to_sync(self.channel_layer.send)(
-            self.channel_name,
-            {
-                "type": "chat.message",
-                "text": {"msg": "Bot says hello", "source": "bot"},
-            },
-        )
-
-    # Handles the chat.mesage event i.e. receives messages from the channel layer
-    # and sends it back to the client.
     def chat_message(self, event):
         text = event["text"]
         self.send(text_data=json.dumps({"text": text}))
-        
--- core/text2text/routing.py
+++ core/text2text/routing.py
@@ -4,6 +4,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/text2text/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"^ws/text2text/$", consumers.ChatConsumer.as_asgi()),
 ]
-
